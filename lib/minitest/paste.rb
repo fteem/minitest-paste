1ff6cb69482ee5dce31b39407e147c4180b06c5b
Ile Eftimov <ileeftimov@gmail.com>
2016-01-11 20:27:21 +0100

    init

---
+++ lib/minitest/paste.rb
@@ -0,0 +1,7 @@
+require "minitest/paste/version"
+
+module Minitest
+  module Paste
+    # Your code goes here...
+  end
+end
---
+++ lib/minitest/paste/version.rb
@@ -0,0 +1,5 @@
+module Minitest
+  module Paste
+    VERSION = "0.1.0"
+  end
+end
